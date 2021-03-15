51532d7f16912b8bc8659bff87d0a1e8efaf884e
Zackptg5 <5107713+Zackptg5@users.noreply.github.com>
2021-03-15 13:25:53 -0400

    bug fix

--- build_script/build.sh
+++ build_script/build.sh
@@ -633,7 +633,7 @@ build_bin() {
       cd $dir/$bin
       static=$origstatic
       flags="--disable-shared $flags"
-      $STATIC || LDFLAGS="$LDFLAGS -static-libstdc++"
+      $static || LDFLAGS="$LDFLAGS -static-libstdc++"
       ./configure CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS" LIBS="-ldl" \
         --host=$target_host \
         $flags--prefix=/system \
